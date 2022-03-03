8b473ddbbf6a109aefec9667ab3de3ac1d035290
AngelRabasco <rabascoangel01@gmail.com>
2022-03-03 09:05:23 +0100

    Basic classes and ORM

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Account")
+public class Account implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "number")
+	private String number;
+	@Column(name = "balance")
+	private Double balance;
+	@ManyToOne()
+	@JoinColumn(name = "clientId")
+	private Client client;
+
+	public Account(Long id, String number, Double balance, Client client) {
+		this.id = id;
+		this.number = number;
+		this.balance = balance;
+		this.client = client;
+	}
+
+	public Account() {
+		this(Long.MIN_VALUE, "", Double.MIN_VALUE, new Client());
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", number=" + number + ", balance=" + balance + ", client=" + client + "]";
+	}
+
+}
---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Admin")
+public class Admin implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "surname")
+	private String surname;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+
+	public Admin(Long id, String name, String surname, String username, String password) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Admin() {
+		this(Long.MIN_VALUE, "", "", "", "");
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", name=" + name + ", surname=" + surname + ", username=" + username + ", password=" + password + "]";
+	}
+
+}
